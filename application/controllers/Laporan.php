bcaa08ada086c58d71ab6e7e9939e4124de1ff20
bayukresnaadjie <bayukresnaadjie2@gmail.com>
2024-05-27 11:04:31 +0700

    tugas pertemuan 9

--- application/controllers/Laporan.php
+++ application/controllers/Laporan.php
@@ -26,4 +26,39 @@ class Laporan extends CI_Controller
         $data['kategori'] = $this->ModelBuku->getKategori()->result_array();
         $this->load->view('buku/laporan_print_buku', $data);
     }
+    public function laporan_buku_pdf()
+    {
+        $data['buku'] = $this->ModelBuku->getBuku()->result_array();
+        $sroot = $_SERVER['DOCUMENT_ROOT'];
+        include $sroot . "/pustaka-booking/application/third_party/dompdf/autoload.inc.php";
+        $dompdf = new Dompdf\Dompdf();
+        $this->load->view('buku/laporan_pdf_buku', $data);
+        $paper_size = 'A4'; // ukuran kertas
+        $orientation = 'landscape'; //tipe format kertas potrait atau landscape
+        $html = $this->output->get_output();
+        $dompdf->set_paper($paper_size, $orientation);
+        $dompdf->load_html($html);
+        $dompdf->render();
+        $dompdf->stream("laporan_data_buku.pdf", array('Attachment' => 0));
+    }
+    public function export_excel()
+    {
+        $data = array(
+            'title' => 'Laporan Buku',
+            'buku' => $this->ModelBuku->getBuku()->result_array()
+        );
+        $this->load->view('buku/export_excel_buku', $data);
+    }
+    public function laporan_pinjam()
+    {
+        $data['judul'] = 'Laporan Data Peminjaman';
+        $data['user'] = $this->ModelUser->cekData(['email' => $this->session->userdata('email')])->row_array();
+        $data['laporan'] = $this->db->query("select*from pinjam p,detail_pinjam d, buku b, user u where
+       d.id_buku=b.id and p.id_user=u.id and p.no_pinjam=d.no_pinjam")->result_array();
+        $this->load->view('templates/header', $data);
+        $this->load->view('templates/sidebar', $data);
+        $this->load->view('templates/topbar', $data);
+        $this->load->view('pinjam/laporan-pinjam', $data);
+        $this->load->view('templates/footer');
+    }
 }
